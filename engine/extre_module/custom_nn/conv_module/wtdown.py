eade51bdf5b089df964f168bea8b9ebb650d8e77
sober-white <152164331+sober-white@users.noreply.github.com>
2025-11-25 17:52:19 +0800

    Add files via upload

---
+++ engine/extre_module/custom_nn/conv_module/wtdown.py
@@ -0,0 +1,254 @@
+import functools
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+import dill as pickle
+import pywt
+import pywt.data
+
+__all__ = ['WTFusion']
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+    
+class Bottleneck(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+    
+class C3(nn.Module):
+    """CSP Bottleneck with 3 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initialize the CSP Bottleneck with given channels, number, shortcut, groups, and expansion values."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=((1, 1), (3, 3)), e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck with 2 convolutions."""
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+    
+class C3k(C3):
+    """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
+        """Initializes the C3k module with specified channels, number of layers, and configurations."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        # self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+    
+class C2f(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+    
+class C3k2(C2f):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
+        )
+
+def create_wavelet_filter(wave, in_size, out_size, type=torch.float):
+    w = pywt.Wavelet(wave)
+    dec_hi = torch.tensor(w.dec_hi[::-1], dtype=type)
+    dec_lo = torch.tensor(w.dec_lo[::-1], dtype=type)
+    dec_filters = torch.stack([dec_lo.unsqueeze(0) * dec_lo.unsqueeze(1),
+                               dec_lo.unsqueeze(0) * dec_hi.unsqueeze(1),
+                               dec_hi.unsqueeze(0) * dec_lo.unsqueeze(1),
+                               dec_hi.unsqueeze(0) * dec_hi.unsqueeze(1)], dim=0)
+
+    dec_filters = dec_filters[:, None].repeat(in_size, 1, 1, 1)
+
+    # rec_hi = torch.tensor(w.rec_hi[::-1], dtype=type).flip(dims=[0])
+    # rec_lo = torch.tensor(w.rec_lo[::-1], dtype=type).flip(dims=[0])
+    # rec_filters = torch.stack([rec_lo.unsqueeze(0) * rec_lo.unsqueeze(1),
+    #                            rec_lo.unsqueeze(0) * rec_hi.unsqueeze(1),
+    #                            rec_hi.unsqueeze(0) * rec_lo.unsqueeze(1),
+    #                            rec_hi.unsqueeze(0) * rec_hi.unsqueeze(1)], dim=0)
+
+    # rec_filters = rec_filters[:, None].repeat(out_size, 1, 1, 1)
+
+    return dec_filters
+
+def wavelet_transform(x, filters):
+    b, c, h, w = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = F.conv2d(x, filters.to(x.dtype).to(x.device), stride=2, groups=c, padding=pad)
+    x = x.reshape(b, c, 4, h // 2, w // 2)
+    return x
+
+
+def inverse_wavelet_transform(x, filters):
+    b, c, _, h_half, w_half = x.shape
+    pad = (filters.shape[2] // 2 - 1, filters.shape[3] // 2 - 1)
+    x = x.reshape(b, c * 4, h_half, w_half)
+    x = F.conv_transpose2d(x, filters.to(x.dtype).to(x.device), stride=2, groups=c, padding=pad)
+    return x
+
+
+# Define the WaveletTransform class
+class WaveletTransform(Function):
+    @staticmethod
+    def forward(ctx, input, filters):
+        ctx.filters = filters
+        with torch.no_grad():
+            x = wavelet_transform(input, filters)
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad = inverse_wavelet_transform(grad_output, ctx.filters)
+        return grad, None
+
+# Define the InverseWaveletTransform class
+class InverseWaveletTransform(Function):
+    @staticmethod
+    def forward(ctx, input, filters):
+        ctx.filters = filters
+        with torch.no_grad():
+            x = inverse_wavelet_transform(input, filters)
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad = wavelet_transform(grad_output, ctx.filters)
+        return grad, None
+
+# Initialize the WaveletTransform
+def apply(input, filters):
+    return WaveletTransform.apply(input, filters)
+
+def wavelet_transform_init(filters):
+    return functools.partial(apply, filters=filters)
+
+# Initialize the InverseWaveletTransform
+# def inverse_wavelet_transform_init(filters):
+#     def apply(input):
+#         return InverseWaveletTransform.apply(input, filters)
+#     return apply
+
+
+
+class _ScaleModule(nn.Module):
+    def __init__(self, dims, init_scale=1.0, init_bias=0):
+        super(_ScaleModule, self).__init__()
+        self.dims = dims
+        self.weight = nn.Parameter(torch.ones(*dims) * init_scale)
+        self.bias = None
+    
+    def forward(self, x):
+        return torch.mul(self.weight, x)
+    
+    
+
+class WTConv(nn.Module):
+    def __init__(self, in_channels, wt_levels=1, wt_type='db1'):
+        super(WTConv, self).__init__()
+
+        self.in_channels = in_channels
+        self.wt_levels = wt_levels
+        self.wt_filter = create_wavelet_filter(wt_type, in_channels, in_channels, torch.float)
+        self.wt_filter = nn.Parameter(self.wt_filter, requires_grad=False)
+        self.wt_function = wavelet_transform_init(self.wt_filter)
+
+    def forward(self, x):
+
+        wt_x = None
+        curr_x_ll = x
+
+        for i in range(self.wt_levels):
+            curr_shape = curr_x_ll.shape
+            if (curr_shape[2] % 2 > 0) or (curr_shape[3] % 2 > 0):
+                curr_pads = (0, curr_shape[3] % 2, 0, curr_shape[2] % 2)
+                curr_x_ll = F.pad(curr_x_ll, curr_pads)
+
+            curr_x = self.wt_function(curr_x_ll)
+            curr_x_ll = curr_x[:,:,0,:,:]
+            wt_x = curr_x
+                       
+        shape_x = wt_x.shape
+        curr_x_tag = wt_x.reshape(shape_x[0], shape_x[1] * 4, shape_x[3], shape_x[4])
+
+        return curr_x_tag
+    
+class WTFusion(nn.Module):
+    def __init__(self, c1, c2, c3, c4, c5):
+        super().__init__()
+        self.wt_levels = [2, 2, 1]
+        self.c = [c1, c2, c3]
+        self.wtconv = nn.ModuleList([WTConv(self.c[i], self.wt_levels[i]) for i in range(len(self.c))])
+        self.out_block = C3k2(c1*4+c2*4+c3*4, c4, 1, True)
+
+    def forward(self, x):
+        n = len(self.wt_levels)
+        xwt, x4, x5 = x[0:3], x[3], x[4]
+        for i in range(n):
+            xwt[i] = self.wtconv[i](xwt[i])
+        out = self.out_block(torch.cat([xwt[0], xwt[1], xwt[2]], dim=1))
+        return out
+
